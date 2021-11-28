88432f42e68f399b8ccd5a6bf07f75aa35df1a9a
faizan2307 <36506871+faizan2307@users.noreply.github.com>
2021-11-28 14:00:09 +0530

    Initial commit

---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=LAPTOP-AD8UD956\FIRST; initial catalog=Microservice; persist security info=True; user id=sa; password=Faizan@2307;");
+        }
+    }
+}
